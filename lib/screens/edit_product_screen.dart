39815daacf2b96a0ce0f0ba56f52d7c9cc5aef79
Huỳnh Trí Dũng <dunghuynh1990@gmail.com>
2020-02-06 17:13:36 +0800

    Handling update and delete api

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -94,10 +94,26 @@ class _EditProductScreenState extends State<EditProductScreen> {
       _isLoading = true;
     });
     if (_editedProduct.id != null) {
-      Provider.of<Products>(context, listen: false).updateProduct(_editedProduct.id, _editedProduct);
-      setState(() {
-        _isLoading = false;
-      });
+      try {
+        await Provider.of<Products>(context, listen: false).updateProduct(_editedProduct.id, _editedProduct);
+      } catch (error) {
+        await showDialog<Null>(
+          context: context,
+          builder: (ctx) =>
+              AlertDialog(
+                title: Text('An error occurred!'),
+                content: Text('Something went wrong!'),
+                actions: <Widget>[
+                  FlatButton(
+                    onPressed: () {
+                      Navigator.of(ctx).pop();
+                    },
+                    child: Text('Okay'),
+                  )
+                ],
+              ),
+        );
+      }
     } else {
       try {
         await Provider.of<Products>(context, listen: false).addProduct(_editedProduct);
@@ -118,13 +134,12 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 ],
               ),
         );
-      } finally {
-        setState(() {
-          _isLoading = false;
-        });
-        Navigator.of(context).pop();
       }
     }
+    setState(() {
+      _isLoading = false;
+    });
+    Navigator.of(context).pop();
   }
 
   @override
