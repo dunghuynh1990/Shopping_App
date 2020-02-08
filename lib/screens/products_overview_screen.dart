78f2217b6755a101a255b9a19b4d74cc2dca43db
Huỳnh Trí Dũng <dunghuynh1990@gmail.com>
2020-02-08 23:26:18 +0800

    Updating all fetching to Future Builder

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/cart.dart';
-import 'package:shop_app/providers/products.dart';
 import 'package:shop_app/screens/cart_screen.dart';
 import 'package:shop_app/widgets/app_drawer.dart';
 import 'package:shop_app/widgets/badge.dart';
@@ -19,38 +18,6 @@ class ProductsOverviewScreen extends StatefulWidget {
 
 class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   var _showOnlyFavorites = false;
-  var _isInit = true;
-  var _isLoading = false;
-
-  @override
-  void initState() {
-//    Provider.of<Products>(context).fetchAndSetProducts(); // WON'T WORK
-//    Future.delayed(Duration.zero).then((_){
-//      Provider.of<Products>(context).fetchAndSetProducts();
-//    });
-    super.initState();
-  }
-
-  @override
-  void didChangeDependencies() {
-    // this run after the widget is initialized but before it is rendered
-    // this run more often, multiple times
-    // use this method to call api
-    if (_isInit) {
-      setState(() {
-        _isLoading = true;
-      });
-      Provider.of<Products>(context).fetchAndSetProducts().then((_) {
-        setState(() {
-          _isLoading = false;
-        });
-      });
-    }
-    setState(() {
-      _isInit = false;
-    });
-    super.didChangeDependencies();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -98,11 +65,7 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
         ],
       ),
       drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : ProductsGrid(_showOnlyFavorites),
+      body: ProductsGrid(_showOnlyFavorites),
     );
   }
 }
--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -5,24 +5,41 @@ import 'package:shop_app/widgets/product_item.dart';
 
 class ProductsGrid extends StatelessWidget {
   final bool showFaves;
+
   ProductsGrid(this.showFaves);
+
   @override
   Widget build(BuildContext context) {
-    final productsData = Provider.of<Products>(context);
-    final products = showFaves ? productsData.favoriteItems : productsData.items;
-    return GridView.builder(
-      padding: const EdgeInsets.all(10.0),
-      itemCount: products.length,
-      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
-        value: products[i],
-        child: ProductItem(),
-      ),
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-      ),
+    return FutureBuilder(
+      future: Provider.of<Products>(context, listen: false).fetchAndSetProducts(),
+      builder: (context, dataSnapshot) {
+        if (dataSnapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else {
+          if (dataSnapshot.error != null) {
+            return Center(child: Text('An error occured!'));
+          } else {
+            final productsData = Provider.of<Products>(context, listen: false);
+            final products = showFaves ? productsData.favoriteItems : productsData.items;
+            return Consumer<Products>(
+              builder: (ctx, _, child) => GridView.builder(
+                padding: const EdgeInsets.all(10.0),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  childAspectRatio: 3 / 2,
+                ),
+                itemCount: products.length,
+                itemBuilder: (context, index) => ChangeNotifierProvider.value(
+                  value: products[index],
+                  child: ProductItem(),
+                ),
+              ),
+            );
+          }
+        }
+      },
     );
   }
 }
